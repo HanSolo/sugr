77ab79f97cba529df1a01ad211cf9a80d273c1bd
Gerrit Grunwald <han.solo@mac.com>
2024-12-25 12:45:20 +0100

    Initial Commit

---
+++ Sugr/SugrApp.swift
@@ -0,0 +1,32 @@
+//
+//  SugrApp.swift
+//  Sugr
+//
+//  Created by Gerrit Grunwald on 25.12.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SugrApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
